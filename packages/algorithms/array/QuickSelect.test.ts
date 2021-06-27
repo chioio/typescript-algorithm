1561cad2882aed5b0deb2f73f101bf993507c31b
Tenn Chio <Tenn_Chio@icloud.com>
2021-06-27 21:44:31 +0800

    algorithm: quick select

--- packages/algorithms/array/QuickSelect.test.ts
+++ packages/algorithms/array/QuickSelect.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+import { assertEquals } from 'https://deno.land/std@0.99.0/testing/asserts.ts';
 import { QuickSelectFR } from './QuickSelect.ts';
 
 Deno.test('QuickSelectFR 短数组', () => {
